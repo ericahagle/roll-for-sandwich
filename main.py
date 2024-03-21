12697a881b1793995c248a432f0756d5cb411146
Erica Hagle <ericahagle.dev@gmail.com>
2024-03-20 22:49:05 -0700

    feat: Add main file to play the game

---
+++ main.py
@@ -0,0 +1,40 @@
+# Roll for Sandwich #
+"""User will roll different sided dice for each element of a sandwich, resulting in a potentially very random lunch!"""
+
+# Import setup.py to play the game
+import setup
+
+# Greeting
+print("🎲 🥪 Welcome to Roll for Sandwich, where we let fate decide our lunch! 🥪 🎲")
+print()
+
+# Play the game
+setup.roll_for_sandwich()
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------
+# Nice to haves:
+	# Put all the roll_for_____ functions into one function and then use parameters to differentiate (would require putting print statements outside the functions)
+	# Different print statements for different ratings
+	# Options in the data sets for None
+	# Options in the data sets for Roll Again
+		# Butter the bread and roll again, etc
+	# Options in the data sets for Roll Again With Two Dice (for double options)
+	# Options in the data sets for Dealer's Choice of items (user can select any option from the existing list)
+	# Maybe add startswith so the user can also type 'yeah' or 'nope', etc.
+
+
+# --------------------
+# Extra nice-to-haves:
+	# Option for user to enter their own data sets
+	# Dealer's choice ultimate (user can select any option from the existing list OR type in their own item)
